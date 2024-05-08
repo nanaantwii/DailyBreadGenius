4900a9637dbf8e290622d65b5579d2f02e33f203
nanaantwii <143400442+nanaantwii@users.noreply.github.com>
2024-05-08 15:21:47 +0100

    update send-email.php

--- send-email.php
+++ send-email.php
@@ -1,26 +1,43 @@
 <?php
+// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    // Here you can process the form submission, for example, sending an email
-
-    // Redirect the user to a thank you page after form submission
-    echo "<!DOCTYPE html>
-    <html lang=\"en\">
-    <head>
-        <meta charset=\"UTF-8\">
-        <title>Thank You!</title>
-    </head>
+    // Get form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    
+    // Recipient Email Address
+    $to = "kidwiza007@gmail.com";
+    
+    // Email Subject
+    $subject = "New Message from Contact Form";
+    
+    // Email Headers
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $name <$email>\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    
+    // Email Content
+    $email_content = "
+    <html>
     <body>
-        <h1>Thank You!</h1>
+    <h2>You have received a new message from the contact form</h2>
+    <p><strong>Name:</strong> $name</p>
+    <p><strong>Email:</strong> $email</p>
+    <p><strong>Message:</strong><br>$message</p>
     </body>
-    </html>";
-    exit();
+    </html>
+    ";
+    
+    // Send email
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo "Thank you for contacting us. We will get back to you soon.";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
 } else {
-    // If someone tries to access this script directly without submitting the form, redirect them to the contact page
+    // If the form is not submitted, redirect to the contact page
     header("Location: contact.html");
-    exit();
+    exit;
 }
 ?>
